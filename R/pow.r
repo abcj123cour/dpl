9926b37091b8ee6336ea9dea5b9efb8cf2765e65
Jane Doe <james_karimi@virgilio.it>
2021-08-07 12:37:15 +0100

    cube a number

--- R/pow.r
+++ R/pow.r
@@ -8,18 +8,20 @@
 
 
 
-square <- function (x){
-  return (x^2)
+square <- function(x) {
+    return(x^2)
 }
 
-#' cube a number
+#' A cube function
 #'
 #' takes a numeric value and cubes it
 #'
-#' @param Number to be cubed
+#' @param  x Number to be cubed
 #' @return the cube of the input
 #' @export
 
-cube <- function (x){
-  return (x^3)
+cube <- function(x) {
+    return(x^3)
 }
+
+
