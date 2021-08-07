fbffdb436c644ddaebd00089bce2f42d6d4422d2
Jane Doe <james_karimi@virgilio.it>
2021-08-07 11:15:29 +0100

    upload

---
+++ R/pow.r
@@ -0,0 +1,13 @@
+#' A square function
+#'
+#' takes a numeric value and squares it
+#'
+#' @param x a numeric value
+#' @return The square of x
+#' @export
+
+
+
+square <- function (x){
+  return (x^2)
+}
